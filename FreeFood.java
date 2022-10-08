d6daacc1dd864421842c29f71ea6fefbc5bb439b
Yuki Okamoto <75482698+YukiOkamoto0206@users.noreply.github.com>
2022-10-07 20:09:26 -0700

    add prefix sum solution

--- FreeFood.java
+++ FreeFood.java
@@ -5,17 +5,41 @@ public class FreeFood {
     Scanner scan = new Scanner(System.in);
     int[] events = new int[366];
     int N = scan.nextInt();
+    
+//  brute force: O(N*events.length)
+//     for (int i = 0; i < N; i++) {
+//       int s = scan.nextInt();
+//       int t = scan.nextInt();
+//       for (int j = s; j <= t; j++) {
+//         events[j] = 1;
+//       }
+//     }
+//     int sum = 0;
+//     for (int n: events) {
+//       sum+=n;
+//     }
+//     System.out.println(sum);
+    
+
+// Prefix sum: O(N+events.length)    
     for (int i = 0; i < N; i++) {
       int s = scan.nextInt();
       int t = scan.nextInt();
-      for (int j = s; j <= t; j++) {
-        events[j] = 1;
-      }
+      ++events[s-1];
+      --events[t];
     }
-    int sum = 0;
-    for (int n: events) {
-      sum+=n;
+
+    // prefix sum
+    for (int i = 1; i < events.length; i++) {
+      events[i] += events[i-1];
     }
-    System.out.println(sum);
+
+    int answer = 0;
+    for (int count: events) {
+      if (count > 0 ) {
+        answer++;
+      } 
+    }
+    System.out.println(answer);
   }
 }
